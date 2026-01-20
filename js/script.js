a2eb1ef5d7bbb4987856474b0ce4518a798382ec
earth <earthearth13218@gamil.com>
2026-01-20 13:16:39 +0700

    commit main

---
+++ js/script.js
@@ -0,0 +1,11 @@
+/* ไฟล์: js/script.js */
+
+// ทดสอบว่า JS ทำงาน
+console.log("PeerHire Script Loaded!");
+
+// ฟังก์ชันจำลองเมื่อกดปุ่ม Join Waitlist
+function joinWaitlist() {
+    alert("Thank you for your interest! We've added you to the waitlist.");
+}
+
+// โค้ดสำหรับทำ Effect เมนู หรือ Animation อื่นๆ จะมาใส่ที่นี่ในอนาคต
